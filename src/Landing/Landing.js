e64a2420a837576a5899467ed3f32326304ebeb6
kasDev18 <kascatindoy@yahoo.com>
2022-07-12 23:59:03 +0800

    create cli for landing

---
+++ src/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Landing.module.css';
+
+const Landing = () => (
+  <div className={styles.Landing}>
+    Landing Component
+  </div>
+);
+
+Landing.propTypes = {};
+
+Landing.defaultProps = {};
+
+export default Landing;
