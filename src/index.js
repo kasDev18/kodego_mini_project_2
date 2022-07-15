4d4f0552ad2c353295f433d4442e9e6719cce8c2
Adrian Estopace <estopaceadrian@gmail.com>
2022-07-16 02:19:53 +0800

    added

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Nav from './components/Nav/Nav';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import ProductCards from './Pages/Products/ProductCards/ProductCards';
 import ProductCart from './Pages/Products/ProductCart/ProductCart';
+import Home from './Pages/Home/Home';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <Nav />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/products" element={ProductCards}>
+        Products
+      </Route>
+    </Routes>
     <ProductCart />
     <ProductCards />
-  </React.StrictMode>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
