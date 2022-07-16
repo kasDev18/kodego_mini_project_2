25098f3cd84f44c273ec32f7c8b298485a91622c
kasDev18 <kascatindoy@yahoo.com>
2022-07-16 23:52:53 +0800

    added nav to rooms general solo

--- src/Rooms_General/Solo/Rooms_Solo.js
+++ src/Rooms_General/Solo/Rooms_Solo.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import styles from './Rooms_Solo.module.css'
-// import Navigation from '../components/Nav/Nav';
+import Navigation from '../../components/Nav/Nav';
 
 class Solo extends React.Component {
     render() {
         return (
             <>
-                {/* <Navigation /> */}
+                <Navigation />
                 <div className={styles.carousel_image + ' '}>
                     a
                 </div>
