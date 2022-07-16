ef6ea3a651afe1ae16c86c61a25ac60b52644fc1
Adrian Estopace <estopaceadrian@gmail.com>
2022-07-16 10:25:47 +0800

    fixed bug

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -22,81 +22,99 @@ class Home extends React.Component {
            https://demos.creative-tim.com/paper-kit-react/#/documentation/carousel 
            */}
         </div>
+
+        {/* 
+          Packages
+        */}
         <div>
-          <div className="container mt-5 d-flex ">
-            <img src="tagaytay/tagaytay2.webp" alt="tagaytay" />
-            <div className="ml-5 text-center ">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
-              incidunt iste amet minus fugit veniam pariatur, itaque suscipit
-              repudiandae nam minima sequi possimus corrupti recusandae hic
-              perferendis rerum ullam sint!
+          <div>
+            <div className="container mt-5 d-flex ">
+              <img src="tagaytay/tagaytay2.webp" alt="tagaytay" />
+              <div className="ml-5 text-center ">
+                <h1>Package 1</h1>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+                incidunt iste amet minus fugit veniam pariatur, itaque suscipit
+                repudiandae nam minima sequi possimus corrupti recusandae hic
+                perferendis rerum ullam sint!
+              </div>
             </div>
           </div>
-        </div>{' '}
-        <div>
-          <div className="container mt-5 d-flex ">
-            <div className="ml-5 text-center ">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
-              incidunt iste amet minus fugit veniam pariatur, itaque suscipit
-              repudiandae nam minima sequi possimus corrupti recusandae hic
-              perferendis rerum ullam sint!
+          <div>
+            <div className="container mt-5 d-flex ">
+              <div className="ml-5 text-center ">
+                <h1>Package 2</h1>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+                incidunt iste amet minus fugit veniam pariatur, itaque suscipit
+                repudiandae nam minima sequi possimus corrupti recusandae hic
+                perferendis rerum ullam sint!
+              </div>
+              <img src="tagaytay/tagaytay2.webp" alt="tagaytay" />
             </div>
-            <img src="tagaytay/tagaytay2.webp" alt="tagaytay" />
+          </div>
+          <div>
+            <div className="container mt-5 d-flex ">
+              <img src="tagaytay/tagaytay2.webp" alt="tagaytay" />
+              <div className="ml-5 text-center ">
+                <h1>Package 3</h1>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+                incidunt iste amet minus fugit veniam pariatur, itaque suscipit
+                repudiandae nam minima sequi possimus corrupti recusandae hic
+                perferendis rerum ullam sint!
+              </div>
+            </div>
           </div>
         </div>
+
+        {/* 
+          TESTIMONIALS
+        */}
         <div>
-          <div className="container mt-5 d-flex ">
-            <img src="tagaytay/tagaytay2.webp" alt="tagaytay" />
-            <div className="ml-5 text-center ">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
-              incidunt iste amet minus fugit veniam pariatur, itaque suscipit
-              repudiandae nam minima sequi possimus corrupti recusandae hic
-              perferendis rerum ullam sint!
+          <div className={styles.HomeTestimonials + ' container-md gap-4 mt-5'}>
+            <div>
+              <img
+                className={styles.HomeAvatar}
+                src="tagaytay/tagaytay2.webp"
+                alt="123"
+              />
+
+              <p className={styles.HomeText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos magni, possimus impedit nobis consequatur
+                perspiciatis distinctio repellat architecto quidem rem. Animi
+                maiores aperiam molestias sapiente nobis iste, enim repudiandae
+                numquam!
+              </p>
             </div>
-          </div>
-        </div>
-        <div className={styles.HomeTestimonials + ' container gap-4 mt-5'}>
-          <div>
-            <img
-              className={styles.HomeAvatar}
-              src="tagaytay/tagaytay2.webp"
-              alt="123"
-            />
+            <div>
+              <img
+                className={styles.HomeAvatar}
+                src="tagaytay/tagaytay2.webp"
+                alt="123"
+              />
 
-            <p className={styles.HomeText}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos magni, possimus impedit nobis consequatur perspiciatis
-              distinctio repellat architecto quidem rem. Animi maiores aperiam
-              molestias sapiente nobis iste, enim repudiandae numquam!
-            </p>
-          </div>
-          <div>
-            <img
-              className={styles.HomeAvatar}
-              src="tagaytay/tagaytay2.webp"
-              alt="123"
-            />
+              <p className={styles.HomeText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos magni, possimus impedit nobis consequatur
+                perspiciatis distinctio repellat architecto quidem rem. Animi
+                maiores aperiam molestias sapiente nobis iste, enim repudiandae
+                numquam!
+              </p>
+            </div>
+            <div>
+              <img
+                className={styles.HomeAvatar}
+                src="tagaytay/tagaytay2.webp"
+                alt="123"
+              />
 
-            <p className={styles.HomeText}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos magni, possimus impedit nobis consequatur perspiciatis
-              distinctio repellat architecto quidem rem. Animi maiores aperiam
-              molestias sapiente nobis iste, enim repudiandae numquam!
-            </p>
-          </div>
-          <div>
-            <img
-              className={styles.HomeAvatar}
-              src="tagaytay/tagaytay2.webp"
-              alt="123"
-            />
-
-            <p className={styles.HomeText}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos magni, possimus impedit nobis consequatur perspiciatis
-              distinctio repellat architecto quidem rem. Animi maiores aperiam
-              molestias sapiente nobis iste, enim repudiandae numquam!
-            </p>
+              <p className={styles.HomeText}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Dignissimos magni, possimus impedit nobis consequatur
+                perspiciatis distinctio repellat architecto quidem rem. Animi
+                maiores aperiam molestias sapiente nobis iste, enim repudiandae
+                numquam!
+              </p>
+            </div>
           </div>
         </div>
       </>
