d2e5ae654d2148558871c8df9eaaaa6e61504656
kasDev18 <kascatindoy@yahoo.com>
2022-07-17 02:21:59 +0800

    added footer

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -154,7 +154,37 @@ class Landing extends React.Component {
                         <img src="https://www.bellevueclubhotel.com/wp-content/uploads/2022/01/pms_aaa_4diamond_badge_thumb.png" class="rounded " alt="..." />
                     </div>
                 </div>
-                <div className={styles.hotel_locations + ' bg-dark mt-5'}>
+                <div className={styles.hotel_locations + ' justify-content-center bg-dark mt-5 text-uppercase'}>
+                    <div className={styles.hotel_locations_content}>
+                        <div className=''>
+                            <div className="credits ml-auto d-flex justify-content-center text-light mb-5">
+                                <span className="copyright">
+                                    © {new Date().getFullYear()}, made with{" "}
+                                    <i className="fa fa-heart heart" /> by <span className='text-warning'>7Seven</span> | Luxury Hotel
+                                </span>
+                            </div>
+                        </div>
+                        <div className='d-flex justify-content-center'>
+                            <h4 className='text-white'>Locations</h4>
+                        </div>
+                        <div className='d-flex justify-content-center mb-5'>
+                            <h6 className='text-white'>7seven Tagaytay <span className='text-white mx-2 '> |</span></h6>
+                            <h6 className='text-white'>7seven Boracay <span className='text-white mx-2'> |</span></h6>
+                            <h6 className='text-white'>7seven El Nido </h6>
+                        </div>
+                        <hr className='bg-light mb-5'></hr>
+                        <div className='d-flex justify-content-center'>
+                            <h4 className='text-white'>Quick Links</h4>
+                        </div>
+                        <div className='d-flex justify-content-center'>
+                            <h6 className='text-white'>Careers <span className='text-white mb-2 mx-2'> |</span></h6>
+                            <h6 className='text-white'>Privacy Policy <span className='text-white mx-2'> |</span></h6>
+                            <h6 className='text-white'>Cookie Policy <span className='text-white mx-2'> |</span></h6>
+                            <h6 className='text-white'>Reservation Policy </h6>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.footer}>
 
                 </div>
 
